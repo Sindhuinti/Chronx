00df3f26095dd5e3a92ddacf15b41c7f9d5ac0fc
Sindhu Inti <iamsindhuinti23@gmail.com>
2023-05-15 10:27:25 +0530

    feat: change init cmd

--- cmd/init.go
+++ cmd/init.go
@@ -11,6 +11,8 @@ var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Add user's google calendar oauth token",
 	Long: `Chronx init command is used to set up user's google account oauth to get google calendar token`,
+	Args: cobra.ExactArgs(0),
+	DisableFlagsInUseLine: true,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("init called")
 	},
