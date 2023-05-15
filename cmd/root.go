9d6d87af987c71f37b367b1a92ccb418c7e88ce4
Sindhu Inti <iamsindhuinti23@gmail.com>
2023-05-15 10:02:33 +0530

    feat:Add chronx command

--- cmd/root.go
+++ cmd/root.go
@@ -9,18 +9,18 @@ import (
 
 
 
-// rootCmd represents the base command when called without any subcommands
+
 var rootCmd = &cobra.Command{
 	Use:   "chronx",
 	Short: "A CLI tool to manage your calendar",
 	Long: `Chronx is a CLI tool that offers a set of commands for managing your calendar events with ease!`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	CompletionOptions: cobra.CompletionOptions{
+		DisableDefaultCmd: true,
+	},
+	Version: "1.0.0",
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
+
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -29,15 +29,7 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.chronx.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	
 }
 
 
